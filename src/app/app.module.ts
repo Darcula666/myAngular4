55b766a052f4159c284753d340b0ee5c186e172c
xiaobu <1172504039@qq.com>
2017-06-16 10:18:33 +0800

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
